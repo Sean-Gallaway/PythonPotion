f44e0d98f74873575382bc8f034980ce44515cf4
Potat-OS1 <apotat.os1@gmail.com>
2024-11-20 00:00:01 -0800

    progress on widgets

--- src/animationHandler.py
+++ src/animationHandler.py
@@ -3,33 +3,73 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 from pygame import *
 from pyvidplayer2 import *
 from animationObject import *
+from enum import Enum
 
+# container for all the pre-rendered animations
+class animations(Enum):
+    MIX = "potion_game\\animation\\mix.mp4"
+    CHOP = "potion_game\\animation\\chop.mp4"
+    IDLE = "potion_game\\animation\\idle.mp4"
+currentAnim = animations.IDLE;
+
+# function that sets the current background video
+def anim (a: animations):
+    global vid
+    global winSize
+    global currentAnim
+    vid.stop()
+    vid = Video(a[0].value)
+    vid.resize(winSize)
+    currentAnim = a
+
+
+
+
+# setup window
 winSize = (int(1920/2), int(1080/2))
-
 pygame.init()
 window = pygame.display.set_mode(winSize)
 pygame.display.set_caption("Potion Game")
-vid = Video("potion_game\\animation\\mix.mp4")
+
+# setup background
+vid = Video("potion_game\\animation\\idle.mp4")
 vid.resize( winSize );
 vidSurface = pygame.surface.Surface( winSize )
 
-obj = animObject((winSize[0], winSize[1]/3))
+#
+btn1 = btn("mix", (200, .6*(winSize[1]/3) ))
+btn1.setXY(200, 2.2*winSize[1]/3)
+btn1.bindFunction(anim, animations.MIX)
+#
+
+#
+btn2 = btn("chop", (200, .6*(winSize[1]/3) ))
+btn2.setXY(0, 2.2*winSize[1]/3)
+btn2.bindFunction(anim, animations.CHOP)
+#
+
+# Bottom menu
+obj = menu((winSize[0], winSize[1]/3), manager=layoutManager(horizontal=False))
 obj.setXY(0, 2*winSize[1]/3)
 
-btn = abutton("idle", (200, .6*(winSize[1]/3) ))
-btn.setXY(200, 2.2*winSize[1]/3)
+submenu = menu((500, 200), manager=layoutManager(horizontal=True))
+submenu.addItem(btn1, lock=True)
+submenu.addItem(btn2, lock=True)
+obj.addItem(label("Test", (200, 50) ))
+obj.addItem(submenu)
+#
 
-btn2 = abutton("chop", (200, .6*(winSize[1]/3) ))
-btn2.setXY(0, 2.2*winSize[1]/3)
 
-def anim (path: str):
-    global vid
-    global winSize
-    vid = Video(path[0])
-    vid.resize( winSize );
+'''
+Chop animation:
+    acquired item
+mix animation: 
+finish animation:
+    puff of smoke
+        menu screen popup
+'''
 
-btn.bindFunction(anim, "potion_game\\animation\\idle.mp4")
-btn2.bindFunction(anim, "potion_game\\animation\\chop.mp4")
+
 
 play = True
 while play:
@@ -41,19 +81,26 @@ while play:
             mpos = pygame.mouse.get_pos()
             for btn in buttonList:
                 btn.cursorInBounds(mpos)
+    
     if vid.draw(vidSurface, (0, 0), force_draw=False):
         pygame.display.update()
 
     if not vid.active:
-        vid.restart()
+        if currentAnim == animations.IDLE:
+            vid.restart()
+        else:
+            vid = Video(animations.IDLE.value)
+            currentAnim = animations.IDLE
+            vid.resize(winSize)
+        
 
     window.blit(vidSurface, (0, 0))
     obj.draw(window)
-    btn.setXY(210, 2.2*winSize[1]/3)
-    btn.draw(window)
-    btn2.setXY(0, 2.2*winSize[1]/3)
-    btn2.draw(window)
+    
+    # for bt in buttonList:
+    #     bt.draw(window)
 
+    pygame.display.update()
     pygame.time.wait(16) # around 60 fps
 
 pygame.quit()
--- src/animationObject.py
+++ src/animationObject.py
@@ -4,6 +4,11 @@ from pygame import *
 import pygame
 
 
+buttonList = []
+font = pygame.font.SysFont('Corbel',35) 
+
+
+# base class for anything being drawn to the screen.
 class animObject ():
     x = 0
     y = 0
@@ -11,42 +16,49 @@ class animObject ():
     height = 0
     obj = None
 
+    # constructor
     def __init__ (self, size: tuple):
         self.width = size[0]
         self.height = size[1]
         self.obj = pygame.surface.Surface(size, pygame.SRCALPHA, 32 )
-        self.obj.fill((100, 100, 100, 120))
+        self.obj.fill((0, 0, 0, 180))
 
+    # draws this object to a given surface
     def draw (self, window: pygame.surface.Surface):
         window.blit(self.obj, (self.x, self.y))
 
+    # sets the position of this object using screen space coordinates.
     def setXY (self, dx, dy):
         self.x = dx
         self.y = dy
 
 
-
-buttonList = []
-
-font = pygame.font.SysFont('Corbel',35) 
-class abutton (animObject):
-    func = None
-    funcArgs = None
+# classic label.
+class label (animObject):
     text = None
     textSize = None
 
     # constructor
     def __init__ (self, msg: str, size: tuple):
         super().__init__(size)
-        buttonList.append(self)
         self.obj.fill((100, 100, 100, 255))
         self.text = font.render(msg, True, (0, 0, 0))
         self.textSize = font.size(msg)
 
-    #
+    # draws this object and its text to a given surface.
     def draw (self, window: pygame.surface.Surface):
         window.blit(self.obj, (self.x, self.y))
         window.blit(self.text, (self.x+(self.width/2-self.textSize[0]/2), self.y+(self.height/2-self.textSize[1]/2)))
+
+# classic button, does a thing if you bind a function to it.
+class btn (label):
+    func = None
+    funcArgs = None
+
+    # constructor
+    def __init__ (self, msg: str, size: tuple):
+        super().__init__(msg, size)
+        buttonList.append(self)
 
     # check if the cursor is in the bounds of this button
     def cursorInBounds (self, mouse: list):
@@ -66,3 +78,85 @@ class abutton (animObject):
     def bindFunction (self, func, *args):
         self.func = func
         self.funcArgs = args;
+
+# tells a menu how to layout its children.
+class layoutManager ():
+    paddingX = 1
+    paddingY = 1
+    horizontal = None
+    wrap = False
+
+    def __init__ (self, horizontal = None, wrap = False):
+        self.horizontal = horizontal
+        self.wrap = wrap
+
+    # sets the position of all the animObjects in a list in a way specified by the menu
+    def set (self, objects: list, pos: tuple, size: tuple):
+        strideX = 0
+        strideY = 0
+        for item in objects:
+            if self.horizontal == True:
+                # check if wrapping
+                if self.wrap:
+                    if size[0] < strideX + self.paddingX + item.width:
+                        strideX = 0
+                        strideY += item.height + self.paddingY
+                        item.setXY(pos[0] + strideX, pos[1] + strideY)
+                else:
+                    item.setXY(pos[0] + strideX, pos[1] + strideY)
+                strideX += item.width + self.paddingX
+            elif self.horizontal == False:
+                # check if wrapping
+                if self.wrap:
+                    if size[1] < strideY + self.paddingY + item.height:
+                        strideY = 0
+                        strideX += item.height + self.paddingY
+                        item.setXY(pos[0] + strideX, pos[1] + strideY)
+                else:
+                    item.setXY(pos[0] + strideX, pos[1] + strideY)
+                strideY += item.height + self.paddingY
+            else:
+                pass
+    
+# menu object that holds other animObjects.
+class menu (animObject):
+    objects = None
+    open = True
+    manager = None
+
+    #constructor
+    def __init__ (self, size: tuple, manager = None):
+        super().__init__(size)
+        self.objects = []
+        self.manager = manager
+    
+    # draws the menu and its children if it is open.
+    def draw (self, window: pygame.surface.Surface):
+        if open:
+            super().draw(window)
+            for i in self.objects:
+                i.draw(window)
+
+    # checks if object is within the bounds of this menu.
+    def isWithin (self, obj: animObject):
+        if self.x > obj.x or self.x + self.width < obj.x:
+            return False
+        if self.y > obj.y or self.y + self.width < obj.y:
+            return False
+        return True
+    
+    # adds a child to the menu and sets its position in the layout if managed.
+    def addItem (self, obj: animObject, lock = False):
+        self.objects.append(obj)
+        if self.manager != None:
+            self.manager.set(self.objects, (self.x, self.y), (self.width, self.height))
+        else:
+            if lock and not self.isWithin(obj):
+                obj.setXY(self.x, self.y)
+
+    # sets position of the menu and updates the positions of children if managed.
+    def setXY(self, dx, dy):
+        super().setXY(dx, dy)
+        if self.manager != None:
+            self.manager.set(self.objects, (self.x, self.y), (self.width, self.height))
+
