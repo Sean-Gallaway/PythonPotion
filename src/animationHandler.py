d9b0de0b9b1b8a6c20965cd48466963db01fd867
Potat-OS1 <apotat.os1@gmail.com>
2024-11-19 16:15:59 -0800

    buttons and such

--- src/animationHandler.py
+++ src/animationHandler.py
@@ -1,60 +1,59 @@
-import pygame
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+from pygame import *
 from pyvidplayer2 import *
+from animationObject import *
 
+winSize = (int(1920/2), int(1080/2))
 
 pygame.init()
-window = pygame.display.set_mode((1920, 1080))
+window = pygame.display.set_mode(winSize)
 pygame.display.set_caption("Potion Game")
-vid = Video("C:\\Users\\apota\\Desktop\\Python\\potion_game\\animation\\mix.mp4")
+vid = Video("potion_game\\animation\\mix.mp4")
+vid.resize( winSize );
+vidSurface = pygame.surface.Surface( winSize )
+
+obj = animObject((winSize[0], winSize[1]/3))
+obj.setXY(0, 2*winSize[1]/3)
+
+btn = abutton("idle", (200, .6*(winSize[1]/3) ))
+btn.setXY(200, 2.2*winSize[1]/3)
+
+btn2 = abutton("chop", (200, .6*(winSize[1]/3) ))
+btn2.setXY(0, 2.2*winSize[1]/3)
+
+def anim (path: str):
+    global vid
+    global winSize
+    vid = Video(path[0])
+    vid.resize( winSize );
+
+btn.bindFunction(anim, "potion_game\\animation\\idle.mp4")
+btn2.bindFunction(anim, "potion_game\\animation\\chop.mp4")
 
 play = True
-
 while play:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             vid.stop()
             play = False
-    
-    if vid.draw(window, (0, 0), force_draw=False):
+        if event.type == MOUSEBUTTONDOWN:
+            mpos = pygame.mouse.get_pos()
+            for btn in buttonList:
+                btn.cursorInBounds(mpos)
+    if vid.draw(vidSurface, (0, 0), force_draw=False):
         pygame.display.update()
 
     if not vid.active:
         vid.restart()
 
+    window.blit(vidSurface, (0, 0))
+    obj.draw(window)
+    btn.setXY(210, 2.2*winSize[1]/3)
+    btn.draw(window)
+    btn2.setXY(0, 2.2*winSize[1]/3)
+    btn2.draw(window)
+
     pygame.time.wait(16) # around 60 fps
 
 pygame.quit()
-
-# from tkinter import *
-# from animationObject import *
-
-# def test():
-#     playAnimation()
-
-# frame = 0
-# def playAnimation ():
-#     global frame
-#     if frame > 40:
-#         return
-#     else:
-#         frame += 1
-#         window.after(100, playAnimation)
-
-# # set up our window
-# window = Tk()
-# window.title("Potion Game")
-# window.configure(background="gray")
-# wsize = {"x": int(window.winfo_screenwidth()*.75), "y": int(window.winfo_screenheight()*.75) }
-# wpos = {"x": window.winfo_screenwidth()/2 - wsize["x"]/2, "y": window.winfo_screenheight()/2 -wsize["y"]/2}
-# window.minsize( wsize["x"], wsize["y"] )
-# window.geometry("%dx%d+%d+%d" % (wsize["x"], wsize["y"], wpos["x"], wpos["y"]) )
-
-# print(wsize)
-# print(wpos)
-
-# a = animationObject("potion_game//mix", window)
-# test = Button(window, text="drink", command=test)
-# test.place(x=0, y=0)
-
-# # start the loop
-# window.mainloop()
--- src/animationObject.py
+++ src/animationObject.py
@@ -1,40 +1,68 @@
-# import os
-# from os import walk
-# import tkinter
-# import gobject
-
-# class animationObject ():
-#     currentImage = None
-#     directory = None
-#     label = None
-#     loop = False
-#     currentIndex = 0
-#     frames = {}
-#     window = None
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+from pygame import *
+import pygame
 
 
-#     def __init__ (self, path: str, w, loopAnim = False):
+class animObject ():
+    x = 0
+    y = 0
+    width = 0
+    height = 0
+    obj = None
 
-#         self.loop = loopAnim
-#         self.directory = path
-#         self.window = w
+    def __init__ (self, size: tuple):
+        self.width = size[0]
+        self.height = size[1]
+        self.obj = pygame.surface.Surface(size, pygame.SRCALPHA, 32 )
+        self.obj.fill((100, 100, 100, 120))
+
+    def draw (self, window: pygame.surface.Surface):
+        window.blit(self.obj, (self.x, self.y))
+
+    def setXY (self, dx, dy):
+        self.x = dx
+        self.y = dy
+
+
+
+buttonList = []
+
+font = pygame.font.SysFont('Corbel',35) 
+class abutton (animObject):
+    func = None
+    funcArgs = None
+    text = None
+    textSize = None
+
+    # constructor
+    def __init__ (self, msg: str, size: tuple):
+        super().__init__(size)
+        buttonList.append(self)
+        self.obj.fill((100, 100, 100, 255))
+        self.text = font.render(msg, True, (0, 0, 0))
+        self.textSize = font.size(msg)
+
+    #
+    def draw (self, window: pygame.surface.Surface):
+        window.blit(self.obj, (self.x, self.y))
+        window.blit(self.text, (self.x+(self.width/2-self.textSize[0]/2), self.y+(self.height/2-self.textSize[1]/2)))
+
+    # check if the cursor is in the bounds of this button
+    def cursorInBounds (self, mouse: list):
+        if not (self.x < mouse[0] and mouse[0] < self.x + self.width):
+            return
+        if not (self.y < mouse[1] and mouse[1] < self.y + self.height):
+            return
         
-#         index = 0
-#         for (dirpath, dirnames, files) in walk(path):
-#             for item in files:
-#                 # exclude this file, if this file is in the base folder. exclude all non py files
-#                 if ".png" in item:
-#                     print(item)
-#                     self.frames[index] = tkinter.PhotoImage(file = path+"//"+item)
-#                     # self.frames[index].resize((1920, 1080))
-#                 break
-#             break
+        # try to run the bound function
+        try:
+            self.func(self.funcArgs)
+        except Exception as e:
+            print(e)
+            print("function failed")
 
-#         self.currentImage = self.frames[0]
-#         self.render()
-        
-
-#     def render (self):
-#         global window
-#         self.label = tkinter.Label(self.window, image = self.frames[0], borderwidth = -1, highlightthickness = -1)
-#         self.label.place(x = 0, y = 0)
+    # binds a function to this button to be ran when this button is clicked.
+    def bindFunction (self, func, *args):
+        self.func = func
+        self.funcArgs = args;
